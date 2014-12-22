ad73e229c3a362e9a2b632bbbf89bd68e3571362
Caynan <caynanvls@gmail.com>
2014-12-21 21:11:58 -0500

    added code to solve haystack problem

--- solver.py
+++ solver.py
@@ -4,3 +4,15 @@ def solve_reverse_string(to_reverse):
     ans = {'string': to_reverse[::-1]}
 
     return ans
+
+def solve_haystack(input_dictionary):
+    needle = input_dictionary['needle']
+    haystack = input_dictionary['haystack']
+
+    index = None
+    if needle in haystack:
+        index = haystack.index(needle)
+
+    ans = {'needle': index}
+
+    return ans
